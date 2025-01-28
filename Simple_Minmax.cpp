54d889589a0879bee2df676f7f829107e85b0229
aniruddhapande4 <139002282+aniruddhapande4@users.noreply.github.com>
2025-01-28 15:45:29 +0530

    Update Simple_Minmax.cpp

--- Simple_Minmax.cpp
+++ Simple_Minmax.cpp
@@ -3,12 +3,12 @@ using namespace std;
 int main(){
     vector<int> arr={1000,11,445,1,330,3000};
     int size=arr.size();
-    int maxi=INT_MIN;
-    int mini=INT_MAX;
+    int maxi=arr[0];
+    int mini=arr[0];
     int count=0;
-    for(auto &val:arr){
-        maxi=max(maxi,val);
-        mini=min(mini,val);
+    for(int i=1;i<size;i++){
+        maxi=max(maxi,arr[i]);
+        mini=min(mini,arr[i]);
         count+=2;
     }
     cout<<"Minimum element is "<<mini<<endl;
